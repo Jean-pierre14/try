624a25e97689d16220e4f22ce3d178a0593f6048
User <chiruzabisimwa@gmail.com>
2021-04-20 13:20:58 +0200

    Server configurations

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const exp = require('express'),
     app = exp(),
     PORT = process.env.PORT || 7000
 
+app.use(cors())
+app.use(exp.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.json("Welcome")
+})
 app.listen(PORT, (err) => {
     if (err) error({ message: `Error ${err}`, badge: true })
     success({ message: `Server run on port ${PORT}`, badge: true })
