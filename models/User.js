a2f4013695993f1bdded7132fde8a34b56c99216
User <chiruzabisimwa@gmail.com>
2021-05-19 14:29:51 +0200

    User schema

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+// Members Registration
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    date_of_birth: {
+        type: Date,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cpassword: {
+        type: String,
+        required: false
+    },
+    user_level: {
+        type: String,
+        default: 0
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
