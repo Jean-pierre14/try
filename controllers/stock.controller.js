1a19f30bfd4c5eb2897c920e65a6f1f915cc3e68
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-08-11 12:27:20 +0100

    add: CRUD operator in stock controller

--- controllers/stock.controller.js
+++ controllers/stock.controller.js
@@ -1,18 +1,77 @@
 import Stock from "../models/stock.model.js";
 
 export const getAll = async (req, res) => {
-  const results = await Stock.find();
-  res.json(results);
+  try {
+    const results = await Stock.find();
+    res.json(results);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving stocks", error: error.message });
+  }
 };
-export const getOne = (req, res) => {
-  res.json("Get One");
+
+export const getOne = async (req, res) => {
+  const stockId = req.params.id;
+
+  try {
+    const stock = await Stock.findById(stockId);
+    if (!stock) {
+      return res.status(404).json({ message: "Stock not found" });
+    }
+    res.json(stock);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving stock", error: error.message });
+  }
 };
-export const PostOne = (req, res) => {
-  res.json("Post");
+
+export const postOne = async (req, res) => {
+  try {
+    const newStock = new Stock(req.body);
+    await newStock.save();
+    res.status(201).json(newStock);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating stock", error: error.message });
+  }
 };
-export const PutOne = (req, res) => {
-  res.json("Put");
+
+export const putOne = async (req, res) => {
+  const stockId = req.params.id;
+
+  try {
+    const stock = await Stock.findById(stockId);
+    if (!stock) {
+      return res.status(404).json({ message: "Stock not found" });
+    }
+
+    Object.assign(stock, req.body);
+    await stock.save();
+    res.json(stock);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error updating stock", error: error.message });
+  }
 };
-export const deleteOne = (req, res) => {
-  res.json("Delete");
+
+export const deleteOne = async (req, res) => {
+  const stockId = req.params.id;
+
+  try {
+    const stock = await Stock.findById(stockId);
+    if (!stock) {
+      return res.status(404).json({ message: "Stock not found" });
+    }
+
+    await stock.remove();
+    res.json({ message: "Stock deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting stock", error: error.message });
+  }
 };
